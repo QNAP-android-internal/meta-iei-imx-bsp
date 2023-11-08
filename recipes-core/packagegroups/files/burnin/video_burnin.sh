760b0e89a1c414797ce9916cc77e01db45f8086e
Howard Tsou <howardtsou@ieiworld.com>
2023-11-08 11:00:49 +0800

    recipes-core: packgegroups: change test video path
    find test video in SD card root

--- recipes-core/packagegroups/files/burnin/video_burnin.sh
+++ recipes-core/packagegroups/files/burnin/video_burnin.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-test_video=$1
-if ! [ -f $test_video ];then
+sd_path=`ls /run/media/ |grep ^mmc`
+test_video_path=/run/media/$sd_path/test_video.mp4
+if ! [ -f $test_video_path ];then
 	echo "fail" > /tmp/video_qc.txt
 	exit
 fi
@@ -9,7 +10,7 @@ fi
 echo "fail" > /tmp/video_qc.txt
 while true
 do
-	gst-launch-1.0 filesrc location=$test_video typefind=true \
+	gst-launch-1.0 filesrc location=$test_video_path typefind=true \
 	! video/quicktime ! aiurdemux ! queue max-size-time=0 \
 	! vpudec ! waylandsink window-width=320 window-height=180
 	echo "pass" > /tmp/video_qc.txt	
