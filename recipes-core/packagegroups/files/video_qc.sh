f390d5053a35376240c0d6dabcc51c3ceffa781f
chienming <chienming@iei.world.com>
2024-01-03 15:23:14 +0800

    recipes-core:packagegroups:make video_qc.sh available to yocto and ubuntu

--- recipes-core/packagegroups/files/video_qc.sh
+++ recipes-core/packagegroups/files/video_qc.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
-sd_path=`ls /run/media/ |grep ^mmc`
-test_video_path=/run/media/$sd_path/test_video.mp4
-if ! [ -f $test_video_path ];then
-	echo "fail" > /tmp/video_qc.txt
-	exit
+if [ -f /etc/lsb-release ]; then
+	. /etc/lsb-release
+	OS=$DISTRIB_ID
 fi
 
- gst-launch-1.0 filesrc location=$test_video_path typefind=true \
-! video/quicktime ! aiurdemux ! queue max-size-time=0 \
-! vpudec ! waylandsink
+if [ ${OS} = "Ubuntu" ] ;then
+	sd_path=`ls /media/ubuntu/`
+	test_video_path=/media/ubuntu/$sd_path/test_video.mp4
+
+	if ! [ -f $test_video_path ];then
+		echo "fail" > /tmp/video_qc.txt
+		exit
+	fi
+
+	gst-launch-1.0 playbin uri=file://$test_video_path video-sink="glimagesink"
+else
+	sd_path=`ls /run/media/ |grep ^mmc`
+        test_video_path=/run/media/$sd_path/test_video.mp4
+
+        if ! [ -f $test_video_path ];then
+	        echo "fail" > /tmp/video_qc.txt
+	        exit
+        fi
+
+        gst-launch-1.0 filesrc location=$test_video_path typefind=true \
+        ! video/quicktime ! aiurdemux ! queue max-size-time=0 \
+        ! vpudec ! waylandsink
+fi
 
 sh -c 'dialog --colors --title "Video Test" \
 --no-collapse --yesno "See the vdieo??" 10 50'
