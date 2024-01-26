4af19af33aae38aa4181fe0d9e3bf4ed4283c95c
chienming <chienming@iei.world.com>
2024-01-26 14:41:08 +0800

    packagegroups: files: Modify camera_burnin.sh function
    To distinguish between yocto and ubuntu.

--- recipes-core/packagegroups/files/burnin/camera_burnin.sh
+++ recipes-core/packagegroups/files/burnin/camera_burnin.sh
@@ -9,6 +9,15 @@ else
 	echo "fail" > /tmp/camera_qc.txt
 fi
 
-gst-launch-1.0 v4l2src device=/dev/video3 ! \
-'video/x-raw,format=YUY2,width=1280,height=720,pixel-aspect-ratio=1/1,framerate=30/1' ! \
- waylandsink window-height=180 window-width=320 
+if [ -f /etc/lsb-release ]; then
+        . /etc/lsb-release
+        OS=$DISTRIB_ID
+fi
+
+if [ ${OS} = "Ubuntu" ] ;then
+	sudo gst-launch-1.0 v4l2src device=/dev/video3 ! 'video/x-raw,format=YUY2,width=720,height=480,pixel-aspect-ratio=1/1,framerate=30/1' ! glimagesink
+else
+	gst-launch-1.0 v4l2src device=/dev/video3 ! \
+	'video/x-raw,format=YUY2,width=1280,height=720,pixel-aspect-ratio=1/1,framerate=30/1' ! \
+	 waylandsink window-height=180 window-width=320
+fi
