56bc80f2ce7d8f4e9ef2f969391724cd3f1c0a24
chienming <chienming@iei.world.com>
2024-01-26 15:07:39 +0800

    packagegroups: files: Modify video_burnin.sh function
    To distinguish between yocto and ubuntu.

--- recipes-core/packagegroups/files/burnin/video_burnin.sh
+++ recipes-core/packagegroups/files/burnin/video_burnin.sh
@@ -1,17 +1,38 @@
 #!/bin/sh
 
-sd_path=$(mount | grep mmcblk1 | awk -F' ' '{print $3}')
-test_video_path=$sd_path/test_video.mp4
-if ! [ -f $test_video_path ];then
-	echo "fail" > /tmp/video_qc.txt
-	exit
+echo "fail" > /tmp/video_qc.txt
+
+if [ -f /etc/lsb-release ]; then
+        . /etc/lsb-release
+        OS=$DISTRIB_ID
 fi
 
-echo "fail" > /tmp/video_qc.txt
-while true
-do
-	gst-launch-1.0 filesrc location=$test_video_path typefind=true \
-	! video/quicktime ! aiurdemux ! queue max-size-time=0 \
-	! vpudec ! waylandsink window-width=320 window-height=180
-	echo "pass" > /tmp/video_qc.txt	
-done
+if [ ${OS} = "Ubuntu" ] ;then
+        sd_path=`ls /media/ubuntu/`
+        test_video_path=/media/ubuntu/$sd_path/test_video.mp4
+        if ! [ -f $test_video_path ];then
+                echo "fail" > /tmp/video_qc.txt
+                exit
+        fi
+        while true
+        do
+                sudo gst-launch-1.0 filesrc location=$test_video_path ! qtdemux name=demux demux.video_0 !  vpudec ! glimagesink render-rectangle='<1,1,360,240>'
+                echo "pass" > /tmp/video_qc.txt
+        done
+else
+        sd_path=$(mount | grep mmcblk1 | awk -F' ' '{print $3}')
+	test_video_path=$sd_path/test_video.mp4
+	if ! [ -f $test_video_path ];then
+                echo "fail" > /tmp/video_qc.txt
+                exit
+        fi
+
+        while true
+        do
+                gst-launch-1.0 filesrc location=$test_video_path typefind=true \
+                ! video/quicktime ! aiurdemux ! queue max-size-time=0 \
+                ! vpudec ! waylandsink window-width=320 window-height=180
+                echo "pass" > /tmp/video_qc.txt
+        done
+fi
+
