e44850704e68ea045ef3df29844d133cf04e93cd
chienming <chienming@iei.world.com>
2024-01-29 13:49:56 +0800

    recipes-core: packagegroups: remove sudo in camera_burnin
    Due to /qc/ change owner to ubuntu,so doesn't need to execute by sudo.

--- recipes-core/packagegroups/files/burnin/camera_burnin.sh
+++ recipes-core/packagegroups/files/burnin/camera_burnin.sh
@@ -15,7 +15,7 @@ if [ -f /etc/lsb-release ]; then
 fi
 
 if [ ${OS} = "Ubuntu" ] ;then
-	sudo gst-launch-1.0 v4l2src device=/dev/video3 ! 'video/x-raw,format=YUY2,width=720,height=480,pixel-aspect-ratio=1/1,framerate=30/1' ! glimagesink
+	gst-launch-1.0 v4l2src device=/dev/video3 ! 'video/x-raw,format=YUY2,width=720,height=480,pixel-aspect-ratio=1/1,framerate=30/1' ! glimagesink
 else
 	gst-launch-1.0 v4l2src device=/dev/video3 ! \
 	'video/x-raw,format=YUY2,width=1280,height=720,pixel-aspect-ratio=1/1,framerate=30/1' ! \
