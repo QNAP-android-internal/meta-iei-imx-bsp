fac61f17d36e284d96081e487e345807f6ddec18
chienming <chienming@iei.world.com>
2024-01-29 14:54:25 +0800

    recipes-core: packagegroups: Modify gpio_burnin.sh function
    To distinguish between yocto and ubuntu for do difference command.

--- recipes-core/packagegroups/files/burnin/gpio_burnin.sh
+++ recipes-core/packagegroups/files/burnin/gpio_burnin.sh
@@ -8,18 +8,32 @@ else
 	exit
 fi
 
+if [ -f /etc/lsb-release ]; then
+	. /etc/lsb-release
+	OS=$DISTRIB_ID
+fi
+
 while true
 do
 	# check loopback 1-2
-	gpioset gpiochip5 8=1
-	GET_VAL=$(gpioget gpiochip5 9)
+	if [ "$OS" = "Ubuntu" ]; then
+		sudo gpioset gpiochip5 8=1
+		GET_VAL=$(sudo gpioget gpiochip5 9)
+	else
+		gpioset gpiochip5 8=1
+		GET_VAL=$(gpioget gpiochip5 9)
+	fi
 	if [[ "$GET_VAL" != 1 ]]; then
 		echo "fail" > /tmp/gpio_qc.txt
 		continue
 	fi
-
-	gpioset gpiochip5 8=0
-	GET_VAL=$(gpioget gpiochip5 9)
+	if [ "$OS" = "Ubuntu" ]; then
+		sudo gpioset gpiochip5 8=0
+		GET_VAL=$(sudo gpioget gpiochip5 9)
+	else
+		gpioset gpiochip5 8=0
+		GET_VAL=$(gpioget gpiochip5 9)
+	fi
 	if [[ "$GET_VAL" != 0 ]]; then
 		echo "fail" > /tmp/gpio_qc.txt
 		continue
@@ -27,15 +41,24 @@ do
 
 
 	# check loopback 3-4
-	gpioset gpiochip5 10=1
-	GET_VAL=$(gpioget gpiochip5 11)
+	if [ "$OS" = "Ubuntu" ]; then
+		sudo gpioset gpiochip5 10=1
+		GET_VAL=$(sudo gpioget gpiochip5 11)
+	else
+		gpioset gpiochip5 10=1
+		GET_VAL=$(gpioget gpiochip5 11)
+	fi
 	if [[ "$GET_VAL" != 1 ]]; then
 		echo "fail" > /tmp/gpio_qc.txt
 		continue
 	fi
-
-	gpioset gpiochip5 10=0
-	GET_VAL=$(gpioget gpiochip5 11)
+	if [ "$OS" = "Ubuntu" ]; then
+		sudo gpioset gpiochip5 10=0
+		GET_VAL=$(sudo gpioget gpiochip5 11)
+	else
+		gpioset gpiochip5 10=0
+		GET_VAL=$(gpioget gpiochip5 11)
+	fi
 	if [[ "$GET_VAL" != 0 ]]; then
 		echo "fail" > /tmp/gpio_qc.txt
 		continue
@@ -43,30 +66,50 @@ do
 
 
 	# check loopback 5-6
-	gpioset gpiochip5 12=1
-	GET_VAL=$(gpioget gpiochip5 13)
+	if [ "$OS" = "Ubuntu" ]; then
+		sudo gpioset gpiochip5 12=1
+		GET_VAL=$(sudo gpioget gpiochip5 13)
+	else
+		gpioset gpiochip5 12=1
+		GET_VAL=$(gpioget gpiochip5 13)
+	fi
 	if [[ "$GET_VAL" != 1 ]]; then
 		echo "fail" > /tmp/gpio_qc.txt
 		continue
 	fi
 
-	gpioset gpiochip5 12=0
-	GET_VAL=$(gpioget gpiochip5 13)
+	if [ "$OS" = "Ubuntu" ]; then
+		sudo gpioset gpiochip5 12=0
+		GET_VAL=$(sudo gpioget gpiochip5 13)
+	else
+		gpioset gpiochip5 12=0
+		GET_VAL=$(gpioget gpiochip5 13)
+	fi
 	if [[ "$GET_VAL" != 0 ]]; then
 		echo "fail" > /tmp/gpio_qc.txt
 		continue
 	fi
 
 	# check loopback 7-8
-	gpioset gpiochip5 14=1
-	GET_VAL=$(gpioget gpiochip5 15)
+	if [ "$OS" = "Ubuntu" ]; then
+		sudo gpioset gpiochip5 14=1
+		GET_VAL=$(sudo gpioget gpiochip5 15)
+	else
+		gpioset gpiochip5 14=1
+		GET_VAL=$(gpioget gpiochip5 15)
+	fi
 	if [[ "$GET_VAL" != 1 ]]; then
 		echo "fail" > /tmp/gpio_qc.txt
 		continue
 	fi
 
-	gpioset gpiochip5 14=0
-	GET_VAL=$(gpioget gpiochip5 15)
+	if [ "$OS" = "Ubuntu" ]; then
+		sudo gpioset gpiochip5 14=0
+		GET_VAL=$(sudo gpioget gpiochip5 15)
+	else
+		gpioset gpiochip5 14=0
+		GET_VAL=$(gpioget gpiochip5 15)
+	fi
 	if [[ "$GET_VAL" != 0 ]]; then
 		echo "fail" > /tmp/gpio_qc.txt
 		continue
