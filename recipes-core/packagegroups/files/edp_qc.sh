45124e4814b310ffa3dd4766d3d488ac50f4ddf5
chienming <chienming@iei.world.com>
2024-03-20 17:27:44 +0800

    recipes-core: packagegroups: modify edp_qc.sh for ubuntu
        1.rgb test: to distinguish rgb test between yocto and ubuntu.
        2.screen brightness test: due to Ubuntu must use sudo to control brightness ,
        	so modify command make execute on yocto and ubuntu.

--- recipes-core/packagegroups/files/edp_qc.sh
+++ recipes-core/packagegroups/files/edp_qc.sh
@@ -1,32 +1,42 @@
 #!/bin/sh
+if [ -f /etc/lsb-release ]; then
+	. /etc/lsb-release
+	OS=$DISTRIB_ID
+fi
 
 # RGB test
-weston-presentation-shm > /dev/null 2>&1 &
-RGB_PID=$(pidof weston-presentation-shm)
-sleep 3
-kill -9 $RGB_PID
-weston-presentation-shm > /dev/null 2>&1 &
-RGB_PID=$(pidof weston-presentation-shm)
-sleep 3
-kill -9 $RGB_PID
-weston-presentation-shm > /dev/null 2>&1 &
-RGB_PID=$(pidof weston-presentation-shm)
-sleep 3
-kill -9 $RGB_PID
+if [ "$OS" = "Ubuntu" ]; then
+        gst-launch-1.0 videotestsrc ! videoconvert ! glimagesink > /dev/null 2>&1 &
+        sleep 3
+        RGB_PID=$(pidof gst-launch-1.0 videotestsrc ! videoconvert ! glimagesinkk)
+        kill -9 $RGB_PID
+else
+	weston-presentation-shm > /dev/null 2>&1 &
+	RGB_PID=$(pidof weston-presentation-shm)
+	sleep 3
+	kill -9 $RGB_PID
+	weston-presentation-shm > /dev/null 2>&1 &
+	RGB_PID=$(pidof weston-presentation-shm)
+	sleep 3
+	kill -9 $RGB_PID
+	weston-presentation-shm > /dev/null 2>&1 &
+	RGB_PID=$(pidof weston-presentation-shm)
+	sleep 3
+	kill -9 $RGB_PID
 
-weston-simple-dmabuf-feedback > /dev/null 2>&1
-
+	weston-simple-dmabuf-feedback > /dev/null 2>&1
+fi
 # Backlight test
 BL_DEV=$(cat /proc/device-tree/model | tr -d '\000' | cut -d ' ' -f3)
 
 for i in $(seq 1 100);
 do
     if [ "$BL_DEV" = "B643" ]; then
-        echo "$i" > /sys/class/backlight/dsi_backlight/brightness
+        echo "echo '$i' > /sys/class/backlight/dsi_backlight/brightness" | sudo sh
     elif [ "$BL_DEV" =  "B664" ]; then
-        echo "$i" > /sys/class/backlight/dp_backlight/brightness
+        echo "echo '$i' > /sys/class/backlight/dp_backlight/brightness" | sudo sh
     else
-        echo "$i" > /sys/class/backlight/dsi_backlight/brightness
+       echo "echo '$i' > /sys/class/backlight/dsi_backlight/brightness" | sudo sh
     fi
 
     sleep 0.1
