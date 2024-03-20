741357fd4fa05c01c87edb986f55831227bf8234
chienming <chienming@iei.world.com>
2024-03-20 17:25:36 +0800

    recipes-extended: gnome-setting: disable screen blank
    Disable turn the screen off after a period of inactivity.

--- recipes-extended/gnome-setting/files/gnome-setting.sh
+++ recipes-extended/gnome-setting/files/gnome-setting.sh
@@ -1,3 +1,4 @@
 gsettings set org.gnome.shell enabled-extensions "['no-overview@fthx']"
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
