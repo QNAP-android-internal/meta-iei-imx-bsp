6c6a311900f29250c20568ad6dd613f2383c2aa7
chienming <chienming@iei.world.com>
2024-03-25 11:14:00 +0800

    recipes-extended: gnome-setting: set screen-lock of suspend

--- recipes-extended/gnome-setting/files/gnome-setting.sh
+++ recipes-extended/gnome-setting/files/gnome-setting.sh
@@ -2,3 +2,4 @@ gsettings set org.gnome.shell enabled-extensions "['no-overview@fthx']"
 gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.desktop.screensaver ubuntu-lock-on-suspend false
