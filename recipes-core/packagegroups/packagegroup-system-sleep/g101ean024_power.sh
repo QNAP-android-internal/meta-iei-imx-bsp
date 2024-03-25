4d06621b73a67adea501f14b512033aa07f5edbe
chienming <chienming@iei.world.com>
2024-03-25 11:33:34 +0800

    recipes-core: packagegroups: modify g101ean024_power for ubuntu
    Due to the panel of iaso-imx8mm need fine tune in sleep mode, or it will
        be happened about while screen display issue on yocto and ubuntu.
    
        1.modify echo command to execute on yocto and ubuntu.
        2.modify sleep time to solve panel ui turn on too fast problem on
    ubuntu.

--- recipes-core/packagegroups/packagegroup-system-sleep/g101ean024_power.sh
+++ recipes-core/packagegroups/packagegroup-system-sleep/g101ean024_power.sh
@@ -3,16 +3,16 @@
 if [ "${1}" == "pre" ]; then
     CURRENT_BRIGHTNESS=$(cat /sys/class/backlight/dp_backlight/brightness)
     echo $CURRENT_BRIGHTNESS > /tmp/curr_brightness
-    echo 0 > /sys/class/backlight/dp_backlight/brightness
+    echo "echo 0 > /sys/class/backlight/dp_backlight/brightness" | sudo sh
 fi
 
 if [ "${1}" == "post" ]; then
     sleep 0.8
-    echo off > /sys/class/drm/card1-DSI-1/status
+    echo "echo off > /sys/class/drm/card1-DSI-1/status" | sudo sh
     sleep 0.4
-    echo on > /sys/class/drm/card1-DSI-1/status
+    echo "echo on > /sys/class/drm/card1-DSI-1/status" | sudo sh
 
-    sleep 0.2
+    sleep 0.5
     CURRENT_BRIGHTNESS=$(cat /tmp/curr_brightness)
-    echo "$CURRENT_BRIGHTNESS" > /sys/class/backlight/dp_backlight/brightness
+    echo "echo '$CURRENT_BRIGHTNESS' > /sys/class/backlight/dp_backlight/brightness" | sudo sh
 fi
